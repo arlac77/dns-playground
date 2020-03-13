3b1ca7b6873813206c73d0ca7af11965413e117b
Markus Felten <Markus.Felten@gmx.de>
2020-03-13 20:20:58 +0100

    feat: add backend

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
 import fs from "fs";
 import  dnsz from "dnsz";
-import {Utils, SymbolInternals, SymbolMap, BasicBackend, Diff} from "SymatemJS";
+import {Utils, SymbolInternals, SymbolMap, BasicBackend, Diff, RustWasmBackend} from "SymatemJS";
+
 
 
 async function doit() {
+    const backend = new RustWasmBackend();
+    backend.initPredefinedSymbols();
+
     const data = dnsz.parse(await fs.promises.readFile("tests/fixtures/private.zone", { encoding: "utf8"}));
 
     console.log(data);
