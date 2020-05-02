febf60395f9a9f07d121d300ba8a913f1e8713f5
Markus Felten <Markus.Felten@gmx.de>
2020-05-02 14:37:32 +0200

    fix: mode queryTriples into SymatemQuery

--- tests/queries-test.mjs
+++ tests/queries-test.mjs
@@ -1,35 +1,8 @@
 import test from "ava";
 import { prepareBackend } from "./helpers/util.mjs";
 
-import { traverse, tripleQueries, DeclareVariable } from "../src/util.mjs";
+import { traverse } from "../src/util.mjs";
 
-test.only("tripleQueries", async t => {
-  const { writer, recordingNamespace } = await prepareBackend();
-
-  const a1 = writer.createSymbol(recordingNamespace);
-  const a2 = writer.createSymbol(recordingNamespace);
-  const s1 = writer.createSymbol(recordingNamespace);
-  const s2 = writer.createSymbol(recordingNamespace);
-  const s3 = writer.createSymbol(recordingNamespace);
-
-  writer.setTriple([s1, a1, s2], true);
-  writer.setTriple([s2, a2, s3], true);
-  //writer.setTriple([s1, a2, s3], true);
-
-  for (const p of tripleQueries(writer, [
-    [s1, a1, DeclareVariable("A")],
-    [DeclareVariable("A"), a2, DeclareVariable("B")]
-  ])) {
-    console.log(p);
-    t.deepEqual(
-      p,
-      new Map([
-        [DeclareVariable("A"), [s2]],
-        [DeclareVariable("B"), [s3]]
-      ])
-    );
-  }
-});
 
 test("traverse", async t => {
   const { writer, recordingNamespace } = await prepareBackend();
